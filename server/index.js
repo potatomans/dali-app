3a2d377058cbaa2cae16017af0bb37b65efc3f68
Kai Shen Siew <kaishensiew@Kais-MacBook-Pro.local>
2025-01-25 17:13:32 -0500

    fix: add build to backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import { getJoke, getMemes, getMemeJoke } from './helper.js';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 
 const memes = await getMemes(); // Get all possible memes from API
 
